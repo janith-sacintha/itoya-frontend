8589f865a429a6ce3a2d7a95589d3ad44915e60b
janith <janith.sachintha.dev@gmail.com>
2025-09-04 12:16:56 +0530

    initialization 🚀

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-blue-300 w-full h-screen'>
+        
+      </div>
+      
+    </>
+  )
+}
+
+export default App
